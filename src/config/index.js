f2d83a9e2775c16fe958c91946ead978b95e45e9
Etherio <50423290+etherio97@users.noreply.github.com>
2021-01-27 13:54:38 +0630

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,7 @@
 const firebaseConfig = require("./firebaseConfig.json");
 
-export { firebaseConfig };
+const api = process.env.NODE_ENV === 'production' 
+              ? 'https://neg.etherio.net/api' /* base url for production */
+              : 'http://localhost:3000'; /* base url for development */
+
+export { firebaseConfig, api };

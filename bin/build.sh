0b84ce2a07bf92cce0527a275ed1f1cf8fd07a60
etherio <etherio97@users.noreply.github.com>
2021-01-31 03:21:46 +0630

    c

--- bin/build.sh
+++ bin/build.sh
@@ -4,5 +4,4 @@ npm install
 npm run firebase:config
 node_modules/.bin/vue-cli-service build
 mv dist/ server/
-curl -o server/serviceAccount-config.json --url "$FIREBASE_CREDENTIAL_URL"
 cd server && npm install
--- bin/get-firebase-config.js
+++ bin/get-firebase-config.js
@@ -5,18 +5,64 @@ const { existsSync, writeFile } = require("fs");
 const { join } = require("path");
 const configFile = join(process.cwd(), "src/config/firebase-config.json");
 const configUrl = `https://${FIREBASE_WEBSITE_URL}/__/firebase/init.json`;
-if (existsSync(configFile)) {
-  console.log("[OK] Already existed config file at:", configFile);
-  process.exit(0);
+if (!existsSync(configFile)) {
+  console.log("[-] Downloading firebase config from URL:", configUrl);
+  axios(configUrl)
+    .then(({ data }) =>
+      writeFile(configFile, JSON.stringify(data), "utf-8", (err) => {
+        if (err) {
+          throw new Error(
+            "[ERR] Unable write firebase config to",
+            configFile,
+            err.message
+          );
+        }
+        console.log("[OK] Saved firebase config as", configFile);
+      })
+    )
+    .catch((err) =>
+      console.log(`[ERR] Failed to download firebase config`, err.message)
+    );
+} else {
+  console.log("[OK] Already existed firebase config at:", configFile);
 }
-console.log("[-] Downloading config from URL:", configUrl);
-axios(configUrl)
-  .then(({ data }) =>
-    writeFile(configFile, JSON.stringify(data), "utf-8", (err) => {
-      if (err) {
-        throw new Error("[ERR] Unable write file to", configFile, err.message);
-      }
-      console.log("[OK] Saved file as", configFile);
-    })
-  )
-  .catch((err) => console.log(`[ERR] Failed to download config`, err.message));
+
+const serviceAccountUrl = process.env.FIREBASE_CREDENTIAL_URL;
+const serviceAccountPath = join(
+  process.cwd(),
+  "server",
+  "serviceAccount-config.json"
+);
+if (!existsSync(serviceAccountPath)) {
+  console.log(
+    "[-] Downloading service account credential from URL:",
+    serviceAccountUrl
+  );
+  axios(serviceAccountUrl)
+    .then(({ data }) =>
+      writeFile(serviceAccountPath, JSON.stringify(data), "utf-8", (err) => {
+        if (err) {
+          throw new Error(
+            "[ERR] Unable write file to",
+            configFile,
+            err.message
+          );
+        }
+        console.log(
+          "[OK] Saved service account credential as",
+          serviceAccountPath
+        );
+      })
+    )
+    .catch((err) =>
+      console.log(
+        `[ERR] Failed to download service account credential `,
+        err.message
+      )
+    );
+} else {
+  console.log(
+    "[OK] Already existed service account credential at:",
+    serviceAccountPath
+  );
+}

d43c4fa58339c7abfdb0f520fa5d70df227fbb6f
etherio <etherio97@users.noreply.github.com>
2021-04-02 20:55:46 +0630

    a

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-  transpileDependencies: ["vuetify"],
-  // devServer: {
-  // proxy: {
-  //   "^/api": {
-  //     target: "http://localhost:3000",
-  //   },
-  // },
-  // },
+  transpileDependencies: ["vuetify"]
 };

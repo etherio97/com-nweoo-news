b651bffc295a1e6b4c8a79c251788d047c67d8b6
Shin Maung Maung <ethereal97@gmail.com>
2021-05-10 23:45:57 +0630

    added sitemap

---
+++ src/views/Profile.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <h2 class="h2">သင့်အကောင့်</h2>
+    <v-row>
+      <!-- link with Google -->
+      <v-col cols="6" sm="12">
+        <v-btn block class="red darken-2">
+          <v-icon class="mr-2">mdi-facebook</v-icon>
+        </v-btn>
+      </v-col>
+
+      <!-- link with Facebook -->
+      <v-col cols="6" sm="12">
+        <v-btn block class="blue darken-2">
+          <v-icon class="mr-2">mdi-facebook</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  //
+};
+</script>

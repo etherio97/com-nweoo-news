c9306607ba54c033c964923468b738b084f9487d
Etherio <50423290+etherio97@users.noreply.github.com>
2023-02-10 10:37:20 +0630

    Update index.js

--- api/articles/index.js
+++ api/articles/index.js
@@ -9,7 +9,7 @@ module.exports = (req, res) => {
   axios(url)
     .then(({ data }) => new JSDOM(data))
     .then(({ window: { document } }) =>
-      axios("https://nweoo-developer.herokuapp.com/news/articles/" + id)
+      axios("https://nweoo-developer.onrender.com/news/articles/" + id)
         .then(({ data }) => {
           let datetime = new Date(data.timestamp);
           let content = data.content.split("\n").filter(n => !!n);
